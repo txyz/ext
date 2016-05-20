3652bc7414d419c0b55509fb0b64adc230607a2b
yhw1630 <yhw163@yahoo.com>
2016-05-20 16:55:20 -0700

    Create Point.hpp

---
+++ Point.hpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Point.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/20/2016 04:54:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hongwei Yang (yhw1630), yhw1630@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  EXT_Point_INC
+#define  EXT_Point_INC
+
+#endif   /* ----- #ifndef Point_INC  ----- */
