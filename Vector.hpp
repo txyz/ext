edb8c9c51374e442239e8f5986b58ac18a9866fb
yhw1630 <yhw163@yahoo.com>
2016-05-20 16:57:03 -0700

    Create Vector.hpp

---
+++ Vector.hpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Vector.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/20/2016 04:54:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hongwei Yang (yhw1630), yhw1630@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  EXT_Vector_INC
+#define  EXT_Vector_INC
+
+#endif   /* ----- #ifndef Vector_INC  ----- */
