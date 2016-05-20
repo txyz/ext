d6f22c6e199ca3c76835ba86a0c67fa0d7a78634
yhw1630 <yhw163@yahoo.com>
2016-05-20 16:58:38 -0700

    Create Size.hpp

---
+++ Size.hpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Size.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/20/2016 04:54:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hongwei Yang (yhw1630), yhw1630@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  EXT_Size_INC
+#define  EXT_Size_INC
+
+#endif   /* ----- #ifndef Size_INC  ----- */
