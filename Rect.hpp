18161d035f86c19bf38ac3acf461c306f5ded29a
yhw1630 <yhw163@yahoo.com>
2016-05-20 17:00:06 -0700

    Create Rect.hpp

---
+++ Rect.hpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Rect.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/20/2016 04:54:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hongwei Yang (yhw1630), yhw1630@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  EXT_Rect_INC
+#define  EXT_Rect_INC
+
+#endif   /* ----- #ifndef Rect_INC  ----- */
