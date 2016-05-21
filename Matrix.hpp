b0192bcde03ad168336206e2c18033a4400444e8
yhw1630 <yhw163@yahoo.com>
2016-05-20 17:01:10 -0700

    Create Matrix.hpp

---
+++ Matrix.hpp
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Matrix.hpp
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  05/20/2016 04:54:07 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Hongwei Yang (yhw1630), yhw1630@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#ifndef  EXT_Matrix_INC
+#define  EXT_Matrix_INC
+
+#endif   /* ----- #ifndef Matrix_INC  ----- */
